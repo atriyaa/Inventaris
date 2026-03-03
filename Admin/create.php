410ecce9f162d018808851aef5220d3cbe3be3eb
farizi27 <ahmadfarizie27072006@gmail.com>
2026-03-03 12:11:01 +0700

    perubahan

--- Admin/create.php
+++ Admin/create.php
@@ -32,7 +32,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Data Inventaris</title>
+    <title>silahkan Tambah Data Inventaris</title>
     <link rel="stylesheet" href="../assets/style.css">
     <style>
         body {
